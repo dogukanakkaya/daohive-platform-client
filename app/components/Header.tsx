3809cc13b5a68891b97e05c17851af79d164dcc8
dogukanakkaya <doguakkaya27@hotmail.com>
2023-06-18 16:54:03 +0300

    feat: add search bar and notif icon

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,14 +8,25 @@ export default async function Header() {
   const { data: { user } } = await supabase.auth.getUser()
 
   return (
-    <div className="w-full h-16 px-4 flex items-center justify-between shadow-lg bg-white dark:bg-gray-900 border-b dark:border-gray-700">
-      <div>x</div>
+    <div className="border-l w-full h-16 px-4 flex items-center justify-between gap-5 shadow-lg bg-white dark:bg-gray-900 border-b dark:border-gray-700">
+      <div className="grow">
+        <input className="form-input w-full" type="search" placeholder="Search..." />
+      </div>
+      <div>
+        <span className="relative block w-12 h-12 rounded-full flex-center shadow cursor-pointer bg-gray-200 dark:bg-gray-600 hover:bg-gray-300 dark:hover:bg-gray-700">
+          <i className="bi bi-bell"></i>
+          <span className="absolute right-0.5 top-0.5 flex h-3 w-3">
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
+          </span>
+        </span>
+      </div>
       <div className="relative group/profile" tabIndex={1}>
         <div className="flex items-center gap-4 cursor-pointer group/picture">
-          <Image src={user?.user_metadata.picture} width={50} height={50} className="rounded-full transition group-hover/picture:ring-2 ring-primary ring-offset-2 ring-offset-white dark:ring-offset-gray-900" alt="Avatar" />
+          <Image src={user?.user_metadata.picture} width={48} height={48} className="rounded-full transition group-hover/picture:ring-2 ring-primary ring-offset-2 ring-offset-white dark:ring-offset-gray-900" alt="Avatar" />
           <h3>{user?.user_metadata.name || user?.email} <i className="bi bi-chevron-down text-sm"></i></h3>
         </div>
-        <ul className="absolute right-0 top-16 w-52 rounded-xl shadow bg-white dark:bg-gray-900 hidden group-focus-within/profile:block">
+        <ul className="absolute right-0 top-16 w-full rounded-xl shadow bg-white dark:bg-gray-900 hidden group-focus-within/profile:block">
           <li>
             <Link href="/profile" className="profile-link rounded-t-xl"><i className="bi bi-person-gear"></i> Profile</Link>
           </li>
