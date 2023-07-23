39e95865b7266c7c7a695d6ca1866a06144d9e3c
dogukanakkaya <doguakkaya27@hotmail.com>
2023-07-23 16:48:38 +0300

    feat: implement retry link for apollo client

--- utils/apollo/wrapper.tsx
+++ utils/apollo/wrapper.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { API_GRAPHQL_URL } from '@/config'
 import { ApolloLink, HttpLink } from '@apollo/client'
+import { RetryLink } from '@apollo/client/link/retry'
 import {
   ApolloNextAppProvider,
   NextSSRApolloClient,
@@ -14,17 +15,31 @@ function makeClient() {
     credentials: 'include'
   })
 
+  const retryStatuses = [429, 503]
+  const retryLink = new RetryLink({
+    delay: (_count, operation) => {
+      const context = operation.getContext()
+
+      return 1000 * (context.response.headers.get('Retry-After') || 30)
+    },
+    attempts: {
+      max: 3,
+      retryIf: (error) => !!error && retryStatuses.includes(error.statusCode)
+    }
+  })
+
+  const links: ApolloLink[] = [
+    retryLink,
+    httpLink
+  ]
+
+  if (typeof window === 'undefined') {
+    links.unshift(new SSRMultipartLink({ stripDefer: true }))
+  }
+
   return new NextSSRApolloClient({
     cache: new NextSSRInMemoryCache(),
-    link:
-      typeof window === 'undefined'
-        ? ApolloLink.from([
-          new SSRMultipartLink({
-            stripDefer: true
-          }),
-          httpLink
-        ])
-        : httpLink
+    link: ApolloLink.from(links)
   })
 }
 
