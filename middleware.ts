43a2486f3bd2456c16909f32510d00349acccd8e
dogukanakkaya <doguakkaya27@hotmail.com>
2023-10-12 22:51:28 +0300

    test

--- middleware.ts
+++ middleware.ts
@@ -9,35 +9,33 @@ export async function middleware(req: NextRequest) {
 
   const supabase = createMiddlewareClient({ req, res })
 
-  // supabase sets domain to the current domain, but api lives on a subdomain so we need reset it
-  supabase.auth.onAuthStateChange((event, session) => {
-    if (event === 'SIGNED_IN') {
-      res.cookies.set({
-        name: SUPABASE_COOKIE_NAME,
-        value: req.cookies.get(SUPABASE_COOKIE_NAME)?.value as string,
-        domain: process.env.NODE_ENV === 'development' ? 'localhost' : '.daohive.io',
-        path: '/',
-        maxAge: session?.expires_at
-      })
-    } else if (event === 'SIGNED_OUT') {
-      res.cookies.set({
-        name: SUPABASE_COOKIE_NAME,
-        value: '',
-        domain: process.env.NODE_ENV === 'development' ? 'localhost' : '.daohive.io',
-        path: '/',
-        maxAge: 0
-      })
-    }
-  })
-
   const { data } = await supabase.auth.getSession()
 
-  if (data.session && UNAUTHENTICATED_ROUTES.includes(req.nextUrl.pathname)) {
-    return NextResponse.redirect(new URL('/contracts', req.url))
-  }
+  if (data.session) {
+    if (UNAUTHENTICATED_ROUTES.includes(req.nextUrl.pathname)) {
+      return NextResponse.redirect(new URL('/contracts', req.url))
+    }
 
-  if (!data.session && !UNAUTHENTICATED_ROUTES.includes(req.nextUrl.pathname)) {
-    return NextResponse.redirect(new URL(UNAUTHENTICATED_ROUTES[0], req.url))
+    // supabase sets domain to the current domain, but api lives on a subdomain so we need reset it
+    res.cookies.set({
+      name: 'SUPABASE_COOKIE_NAME',
+      value: req.cookies.get(SUPABASE_COOKIE_NAME)?.value as string,
+      domain: process.env.NODE_ENV === 'development' ? 'localhost' : '.daohive.io',
+      path: '/',
+      maxAge: data.session.expires_at
+    })
+  } else {
+    if (!UNAUTHENTICATED_ROUTES.includes(req.nextUrl.pathname)) {
+      return NextResponse.redirect(new URL(UNAUTHENTICATED_ROUTES[0], req.url))
+    }
+
+    res.cookies.set({
+      name: 'SUPABASE_COOKIE_NAME',
+      value: '',
+      domain: process.env.NODE_ENV === 'development' ? 'localhost' : '.daohive.io',
+      path: '/',
+      maxAge: 0
+    })
   }
 
   return res
